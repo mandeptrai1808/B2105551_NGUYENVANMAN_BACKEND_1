24eddd7a67d74374b47934088d4599540a304833
John Smith <vanman12315@gmail.com>
2024-10-04 14:42:27 +0700

    Cai dat handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const app = require("./app");
 const config = require("./app/config");
-
+const MongoDB = require("./app/utils/mongodb.util");
 // start the server
-const port = config.app.port;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+async function startServer(){
+  try{
+    await MongoDB.connect(config.db.uri);
+    console.log("Connected to the database!!");
+
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch(err){
+    console.log("Cannot connect to the db!", err);
+    process.exit
+  }
+}
+
+startServer();
